b7950d9e04071911878dab602d9b91ddcfac2007
Jalal Kiswani <kiswanij@gmail.com>
2023-06-21 15:29:29 +0300

    UPDATE TO VERSION 7.0.0-SNAPSHOT

--- src/main/java/com/app/logger/JKLoggerExample.java
+++ src/main/java/com/app/logger/JKLoggerExample.java
@@ -2,9 +2,11 @@ package com.app.logger;
 
 import com.jk.core.logging.JKLogger;
 import com.jk.core.logging.JKLoggerFactory;
+import com.jk.core.util.JK;
 
 public class JKLoggerExample {
 	public static void main(String[] args) {
+		JK.init();
 		JKLogger logger=JKLoggerFactory.getLogger(JKLoggerExample.class);
 		String value="Dummy value";
 		logger.trace("Print trace message with value {}", value);
